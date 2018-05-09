ab74fd25ab1a7986c2a1c9e386448709dade0a1d
Ryan Dupay <ryandupay@framgias-MacBook-Pro.local>
2018-05-09 18:53:42 +0800

    userhome page

---
+++ userhome.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>User Home</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="maindiv center">
+            <div id="header">
+                <img src="images/lammylogo.png" width="200" style="margin: 5 0 5 10;">
+                <div class="menubar">
+                    <ul>
+                      <li><a class="active" href="#home">Home</a></li>
+                      <li><a href="#MyPage">My Page</a></li>
+                      <li><a href="#Inquiry">Inquiry</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>     
+    </body>
+</html>
