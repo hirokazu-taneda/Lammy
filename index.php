5e0125bab69b6fe1f6a1940ca8432a02c49b3af1
Shuka Momose <shukamomose@Shuka-no-MacBook-Air.local>
2018-05-09 16:31:01 +0800

    loginadmin

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
+
+
+<!DOCTYPE html>
 <html>
-<head>
-</head>
-<body>
-	<h1>Welcome to Lammy!</h1>
-</body>
+  <head>
+    
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="lammy.css">
+    
+  </head>
+  <body>
+ 
+    <h1>Welcome to Lammy!</h1>
+    <div class="admlogin"><h1>Admin Login</h1>
+     <hr>
+    
+      <form action="admlogin.php" method="POST">
+ 
+
+    <div class="content">
+    
+        <input type="text" placeholder="E-mail" name="email" required><br>
+    <br><input type="password" placeholder="Password" name="psw" required>
+  </div>
+
+
+<br><br>
+      
+        <button type ="submit" class="subb">Login</button><br>
+        
+      </div>
+      </form>
+  </body>
 </html>
