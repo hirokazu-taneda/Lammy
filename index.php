3725cfa82d5e49aeb517c70e8e15b0ad47f7394e
ryandupay <ryandupay@gmail.com>
2018-06-04 15:44:31 +0800

    favicon and restaurant list for users

--- index.php
+++ index.php
@@ -1,34 +1,73 @@
 
+<?php
+session_start();
 
-<!DOCTYPE html>
-<html>
-  <head>
-    
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="lammy.css">
-    
-  </head>
-  <body>
- 
-    <h1>Welcome to Lammy!</h1>
-    <div class="admlogin"><h1>Admin Login</h1>
-     
-    
-      <form action="admlogin.php" method="POST">
- 
+    include 'dblammy.php';
 
-    <div class="content">
-    
-        <input type="text" placeholder="E-mail" name="email" required><br>
-    <br><input type="password" placeholder="Password" name="psw" required>
-  </div>
+        if (isset($_POST["submit"])){
 
-
-<br><br>
+        $email = $_POST["email"];
+        $pass1 = $_POST["pass1"];
       
-        <button type ="submit" class="subb">Login</button><br>
-        
-      </div>
-      </form>
-  </body>
+            $sql ="SELECT * FROM userinfo WHERE email = '$email' AND pass1 = '$pass1'";
+            $result = $conn->query($sql);
+
+            if($result->num_rows > 0)
+        {
+            while ($row = $result->fetch_assoc()){
+               
+                $_SESSION["email"] = $row['email'];
+                $_SESSION["pass1"] = $row['Pass1'];
+            
+            echo "Hello, " . $_SESSION["name"] . "! Nice to see you today!<br>";
+            echo "<a href='logout.php'><button>Logout</button></a>";
+            }
+
+        } else
+
+            { echo "<a href='maypage.php'>Login</a>";
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>User Login</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="maindiv center">
+        <div id="header">
+            <img src="images/lammylogo.png" width="200" style="margin: 5 0 5 10;">
+                <div class="adminbutton">
+                    <input type="button" class="mainbutton register admin" value="Admin" onClick="location.href='loginadm.php'">
+                </div>
+        </div>
+        <hr>
+        <div class="login">
+            <h1>Login</h1>
+                <form action="loginuser.php" method="POST">
+                    <input type="text" name="email" placeholder="E-mail" required><br>
+                    <input type="password" name="pass1" placeholder="Password" required><br><br>
+                
+                    <button type="submit" class="mainbutton submit" onClick="location.href='userhome.php'">Login</button><br><br>
+                </form>
+                    
+                    <b>If you forget your password<a href="forgetpass.php"> Click</a>here.</b><br><br>
+
+                        <input type="button" class="mainbutton register" value="Register" onClick="location.href='register.php'">
+        </div>
+    </div>
+</body>
 </html>
+
+
+
+
+
+
+
+
+
