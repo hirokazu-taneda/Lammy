7de0a0c09cc3c43779f4c139b8684ed7fa0b204f
種田裕一 <hirokazutaneda@tanetayuuichinoMacBook-Air.local>
2018-06-08 16:28:19 +0800

    register

--- register.php
+++ register.php
@@ -14,25 +14,108 @@ $error = 0;
   $age = $_POST["age"];
   $gender = $_POST["gender"];
   $question = $_POST["question"];
+  $upfile = $_POST["upfile"];
 
+
+                  
+                  $regist_name_error = "";
+                  $regist_pass_error = "";
+                  $regist_q_error = "";
+                  $regist_upfile_error ="";
+
+                  // $fileType = pathinfo($_FILES["upfile"]["name"], PATHINFO_EXTENSION);
+                  // $target_file = $target_dir . basename($_FILES["fileToUpload"]["size"]);
+
+                  unset($regist_name_error, $regist_pass_error, $regist_q_error, $regist_upfile_error);
+
+                  if(isset($_POST["submit"])) {
+                  if($upfile> 2097152) {
+                        $regist_upfile_error = "Sorry, your file is too large.";
+                        $error = 1;
+                      }                  
+                  
+
+                  if($upfile != "jpg" && $upfile != "jpeg" && $upfile != "png") {
+                        $regist_upfile_error = "Please set JPG or JPEG or PNG.";
+                        $error = 1;
+                       }
+                  }
+
+
+                  if(mb_strlen($name) < 2) {
+
+                      $regist_name_error = " Please set name minimum of 2 characters.";
+                      $error = 1;
+                      } elseif(mb_strlen($name) > 10) {
+
+                      $regist_name_error = " Please set name maximum of 10 characters.";
+                        $error = 1;
+                      } elseif (!preg_match("/^[a-zA-Z0-9]+$/", $name)) {
+                      $regist_name_error = "Please type name using half size alphanumeric characters.";
+                      $error = 1;
+                      
+                      } 
   
+                      
+                  if(empty($pass1) || empty($pass2)) {  //if pass1 or pass2 is empty
+        
+                      $regist_pass_error = "Please set your password";
+                      $error = 1;
+                      } 
+                      elseif (mb_strlen($pass1) < 6) {
+                      $regist_pass_error = " Please set password minimum of 6 characters.";
+                      $error = 1;
+                      } 
+                      elseif (mb_strlen($pass1) > 10 ) {
+                      $regist_pass_error = "Please set password maximum of 10 characters.";
+                      $error = 1;
+                      } 
+                      elseif($pass1 !== $pass2) {
+                      $regist_pass_error = "Password does not match. Please set same password in confirm password field.";
+                      $error = 1;
+                      } 
+                      elseif (!preg_match("/^[a-zA-Z0-9]+$/", $pass1)) {
+                      $regist_pass_error = "Please type password using half size alphanumeric characters.";
+                      $error = 1;
+                      } 
 
-  if ($pass1 == $pass2) {
-      $sql = "INSERT INTO userinfo(name, email, pass1, age, gender, question, upfile)
-      VALUES ('$name', '$email', '$pass1', '$age', '$gender', '$question', '')";
 
-      if($conn ->query($sql)){
-       echo "New record created successfully";
-    } else {
-      echo "Error:" . $sql . "<br>" . $conn->error;
-    }
+                  if(mb_strlen($question) < 2) {
 
-  } else {
-    $error = 1;
+                      $regist_q_error = " Please set favorite food minimum of 2 characters.";
+                      $error = 1;
+                      } 
+
+                      elseif(mb_strlen($question) > 10) {
+                      $regist_q_error = " Please set favorite food maximum of 10 characters.";
+                      $error = 1;
+                      } 
+
+                      elseif (!preg_match("/^[a-zA-Z0-9]+$/", $question)) {
+                      $regist_q_error = "Please input type of food using half size alphanumeric characters.";
+                      $error = 1;
+                      } 
+
+               
+
+
+
+                  if ($error == 0 && $pass1 == $pass2) {
+                      $sql = "INSERT INTO userinfo(name, email, pass1, age, gender, question, upfile)
+                      VALUES ('$name', '$email', '$pass1', '$age', '$gender', '$question', '$upfile')";
+
+                  if($conn ->query($sql)){
     
-  }    
+                      header("Location: loginuser.php");
+                      } else {
+                      echo "Error:" . $sql . "<br>" . $conn->error;
+                      }
+                      } else {
+                      $error = 1;
+    
+                      }    
 
-}
+                      }
 ?>
 
 
@@ -50,51 +133,72 @@ $error = 0;
         </div>
         <hr>
         <div class="login">
-			     <h1>Welcome to Register</h1>
+			     <h1>Welcome to Lammy!</h1>
     			 <form action="register.php" method="POST">
         			<div class="form"><br>
-        				<input type="file" name="upfile" size="400" value="picture" class= "pic" ><br>
-        				<p>upload your pic here!</p>
+        				<h3>Profile Photo</h3><br>
+                <input type="file" name="upfile" size="400" value="picture" class= "pic" required><br>
+        				  
         					
                   <input type="text" name="name" placeholder="Name" class= "name" required><br>
+
+              
         					
-                  <input type="text" name="email" placeholder="E-mail" class= "email" required><br>
+                  <input type="email" name="email" placeholder="Email" class= "email" required><br>
                   
         					
                   <input type="password" name="pass1" placeholder="Password" class="pass" required><br>
                   
         					
                   <input type="password" name="pass2" placeholder="comfirm password" required><br><br>
-                  
-                  <?php
-                    if ($error == 1) {
-                      echo "Password does not match. Please set same password in confirm password.<br><br>";
-                    }
 
-                  ?>
-          
-            					<select name="age" required>
-           							<option value="" disabled selected>Age</option>
-              						<option value="u19" >Under 19</option>
-              						<option value="20_24">20~24</option>	
-              						<option value="25_29">25~29</option>
-              						<option value="30_34">30~34</option>
-              						<option value="35_39">35~39</option>
-              						<option value="over40">over 40</option>
-                          
-          						</select><br><br>
         
         					<input type="radio" name="gender" value="male" required>Male
-            			<input type="radio" name="gender" value="female">Female<br>
-                  
+            			<input type="radio" name="gender" value="female">Female
 
+                    <select name="age" required>
+                      <option value="" disabled selected>Age</option>
+                        <option value="u19" >Under 19</option>
+                        <option value="20_24">20~24</option>  
+                        <option value="25_29">25~29</option>
+                        <option value="30_34">30~34</option>
+                        <option value="35_39">35~39</option>
+                        <option value="over40">Over 40</option>
+                          
+                    </select><br><br>
+                  
         					
         					<input type="text" name="question" class= "login" placeholder="What is your favorite food?" required><br>
                   
         			</div>
+                    <?php
+
+                  if (isset($upfile) > 200000) {
+                  echo "<span style='color:#ff0000;'>Sorry, your file is too large.</span><br>";
+                  }
+
+                  
+                  if (isset($upfile) != "jpg" && isset($upfile) != "jpeg" && isset($upfile) != "png") {
+                  echo "<span style='color:#ff0000;'>Please set JPG or JPEG or PNG.</span><br>";
+                  }  
+                    
+                  if (isset($regist_name_error)) {
+                  echo "<span style='color:#ff0000;'>$regist_name_error</span><br>";
+                  }
+                  
+                  if (isset($regist_pass_error)) {
+                  echo "<span style='color:#ff0000;'>$regist_pass_error</span><br>";
+                  }
+                  
+                  if (isset($regist_q_error)) {
+                  echo "<span style='color:#ff0000;'> $regist_q_error</span><br>";
+                  }
+
+                  
+                  ?>
     			
     		<br>
-        	   <button type ="submit" class="mainbutton submit" name="submit">Register</button>
+        	   <button type ="submit" class="mainbutton submit" name="submit" value="submit" >Register</button>
           	 <button type ="reset" class="mainbutton register">Reset</button><br>
             </form>
         </div>
