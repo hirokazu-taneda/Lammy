1bd539a717e2309889aca26ccab9cdbb47159ae0
Shuka Momose <winwin313k@gmail.com>
2018-06-10 15:35:36 +0800

    I couldn't make profile picture

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+  session_start();
   include 'dblammy.php';
 
 
@@ -14,26 +15,52 @@ $error = 0;
   $age = $_POST["age"];
   $gender = $_POST["gender"];
   $question = $_POST["question"];
-
+  $upfile = $_POST["upfile"];
                   
                   $regist_name_error = "";
                   $regist_pass_error = "";
                   $regist_q_error = "";
+                  $regist_upfile_error = "";
 
-                  unset($regist_name_error, $regist_pass_error, $regist_q_error);
+                  $target_dir = "uploads/";
+                  $target_file = $target_dir . basename($_FILES["upfile"]);
+                  $uploadOk = 1; //error =1;
+                  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                  $check = getimagesize($_FILES["upfile"]);
 
+                  unset($regist_name_error, $regist_pass_error, $regist_q_error, $regist_upfile_error);
+
+                  
+                  if($regist_upfile_error !== false) {
+                      $regist_upfile_error = " Its wrong file";
+                      $error = 1; 
+                      } 
+                      elseif(file_exists($target_file)) {
+                      $regist_upfile_error = "Sorry, file already exists.";
+                      $error = 1;
+                      }
+                      elseif($_FILES["upfile"] > 200000) {
+                      $regist_upfile_error = "Sorry, your file is too large.";
+                      $error = 1;
+                      }
+                      elseif($imageFileType != "jpg" && $imageFileType !="png") {
+                      $regist_upfile_error ="Sorry, only JPG & PNG files are allowed.";
+                      $error = 1;
+                      }
+
+
+                  
                   if(mb_strlen($name) < 2) {
-
                       $regist_name_error = " Please set name minimum of 2 characters.";
                       $error = 1;
-                      } elseif(mb_strlen($name) > 10) {
-
+                      } 
+                      elseif(mb_strlen($name) > 10) {
                       $regist_name_error = " Please set name maximum of 10 characters.";
-                        $error = 1;
-                      } elseif (!preg_match("/^[a-zA-Z0-9]+$/", $name)) {
+                      $error = 1;
+                      } 
+                      elseif (!preg_match("/^[a-zA-Z0-9]+$/", $name)) {
                       $regist_name_error = "Please type name using half size alphanumeric characters.";
                       $error = 1;
-                      
                       } 
   
                       
@@ -76,13 +103,10 @@ $error = 0;
                       $error = 1;
                       } 
 
-               
-
-
 
                   if ($error == 0 && $pass1 == $pass2) {
                       $sql = "INSERT INTO userinfo(name, email, pass1, age, gender, question, upfile)
-                      VALUES ('$name', '$email', '$pass1', '$age', '$gender', '$question', '')";
+                      VALUES ('$name', '$email', '$pass1', '$age', '$gender', '$question', 'upfile')";
 
                   if($conn ->query($sql)){
     
@@ -115,24 +139,20 @@ $error = 0;
         <hr>
         <div class="login">
 			     <h1>Welcome to Register</h1>
-    			 <form action="register.php" method="POST">
+    			 <form action="register.php" method="POST" enctype="multipart/form-data">
         			<div class="form"><br>
-        				<input type="file" name="upfile" size="400" value="picture" class= "pic" ><br>
-        				<p>upload your pic here!</p>
-        					
-                  <input type="text" name="name" placeholder="Name" class= "name" required><br>
+                <p>Profile Photo</p>
 
-              
+        				<input type="file" name="upfile" size="200000" value="picture"  ><br><br>
         					
-                  <input type="email" name="email" placeholder="E-mail" class= "email" required><br>
+                  <input type="text" name="name" placeholder="Name" required><br>
+              	
+                  <input type="email" name="email" placeholder="E-mail"  required><br>
                   
-        					
-                  <input type="password" name="pass1" placeholder="Password" class="pass" required><br>
-                  
+                  <input type="password" name="pass1" placeholder="Password"  required><br>
         					
                   <input type="password" name="pass2" placeholder="comfirm password" required><br><br>
 
-        
         					<input type="radio" name="gender" value="male" required>Male
             			<input type="radio" name="gender" value="female">Female
 
@@ -146,13 +166,16 @@ $error = 0;
                         <option value="over40">over 40</option>
                           
                     </select><br><br>
-                  
         					
         					<input type="text" name="question" class= "login" placeholder="What is your favorite food?" required><br>
                   
         			</div>
-                    <?php
 
+                  <?php
+
+                  if (isset($regist_upfile_error)) {
+                  echo "<span style='color:#ff0000;'> $regist_q_error</span><br>";
+                  }
                     
                   if (isset($regist_name_error)) {
                   echo "<span style='color:#ff0000;'>$regist_name_error</span><br>";
@@ -165,20 +188,16 @@ $error = 0;
                   if (isset($regist_q_error)) {
                   echo "<span style='color:#ff0000;'> $regist_q_error</span><br>";
                   }
+
                   ?>
-    			
-    		<br>
+    			   <br>
         	   <button type ="submit" class="mainbutton submit" name="submit" >Register</button>
-          	 <button type ="reset" class="mainbutton register">Reset</button><br>
+          	 
+             <button type ="reset" class="mainbutton register">Reset</button><br>
+            
             </form>
         </div>
+    </div>
 
-    </div>    	
 </body>
 </html>
-
-
-
-
-
-
